98d9ca415b4670784f9f39013e59d5bdc5eb2227
Nik <68340882+nikhilgarg2797@users.noreply.github.com>
2023-04-10 00:11:37 +0530

    Dutch national flag problem solutions

--- dutch_national_flag.cc
+++ dutch_national_flag.cc
@@ -1,16 +1,40 @@
 #include <array>
 #include <vector>
+#include <algorithm>
 
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
 using std::vector;
+using std::swap;
 enum class Color { kRed, kWhite, kBlue };
 
 void DutchFlagPartition(int pivot_index, vector<Color>* A_ptr) {
-  // TODO - you fill in here.
-  return;
+    vector<Color>& A = *A_ptr;
+    Color pivot = A[pivot_index];
+    /**
+     * Keep the following invariants during partitioning:
+     * bottom group: A[0, smaller - 1].
+     * middle group: A[smaller, equal - 1].
+     * unclassified group: A[equal, larger - 1].
+     * top group: A[larger, size(A) - 1].
+     */
+    int smaller = 0, equal = 0, larger = size(A);
+    // Keep iterating as long as there is an unclassified element.
+    while (equal < larger) {
+        // A[equal] is the incoming unclassified element.
+        if (A[equal] < pivot) {
+            swap(A[smaller++], A[equal++]);
+        }
+        else if (A[equal] == pivot) {
+            ++equal;
+        }
+        else {  // A[equal] > pivot.
+            swap(A[equal], A[--larger]);
+        }
+    }
 }
+
 void DutchFlagPartitionWrapper(TimedExecutor& executor, const vector<int>& A,
                                int pivot_idx) {
   vector<Color> colors;
