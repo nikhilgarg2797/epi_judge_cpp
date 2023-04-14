59b90465d530d935da7cde4aaad6adca1180ea06
Nik <68340882+nikhilgarg2797@users.noreply.github.com>
2023-04-15 00:50:33 +0530

    advanced by offset problem solved
    https://www.youtube.com/watch?v=jH_5ypQggWg&t=2s
    look at this video for better understanding. requires comments

--- advance_by_offsets.cc
+++ advance_by_offsets.cc
@@ -2,9 +2,29 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::min;
+using std::max;
 bool CanReachEnd(const vector<int>& max_advance_steps) {
-  // TODO - you fill in here.
-  return true;
+    int n = max_advance_steps.size();
+    vector<int> minimum_jump_array(n, INFINITY);
+    minimum_jump_array[0] = 0;
+    vector<int> jump_path_array;
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (i <= j + max_advance_steps[j])
+            {
+                if (minimum_jump_array[j] + 1 < minimum_jump_array[i])
+                {
+                    jump_path_array.push_back(j);
+                } 
+                minimum_jump_array[i] = min(minimum_jump_array[j]+1, minimum_jump_array[i]);
+            }
+        }
+    }
+
+  return jump_path_array.size()>=n-1;
 }
 
 int main(int argc, char* argv[]) {
